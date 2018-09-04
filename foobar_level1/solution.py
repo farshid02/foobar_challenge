26fad28b9dee2114521667ef4bb8e1c94aa24778
Farshid Shariatzadeh <farshid.shariatzadeh@gmail.com>
2018-09-03 20:05:29 -0700

    Doc string revision

--- foobar_level1/solution.py
+++ foobar_level1/solution.py
@@ -1,9 +1,9 @@
 import math
 def answer(area):
-    '''Write a function answer(area) that takes as its input 
-    a single unit of measure representing the total area of solar panels 
-    you have (between 1 and 1000000 inclusive) and returns a list of 
-    the areas of the largest squares you could make out of those panels, 
+    '''this function takes as its input  a single unit of measure 
+    representing the total area of solar panels  you have (between 1 
+    and 1000000 inclusive) and returns a list of the areas of the 
+    largest squares you could make out of those panels, 
     starting with the largest squares first. So, following the example above, 
     answer(12) would return [9, 1, 1, 1]. '''
     res = []
